a07316d09301e02c82d8d265b2a759642391c8f2
Ashwini <ashwiniparaye9309@gmail.com>
2023-02-15 19:03:44 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import LogIn from './pages/LogIn.jsx';
+import SignUp from './pages/SignUp.jsx';
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/log-in" element={<LogIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route> 
+      </Routes>
+    </Router>
   )
 }
 
