5e27693bcb452adaa2e747e21f425d7cac553e3d
Ashwini <ashwiniparaye9309@gmail.com>
2023-02-16 21:17:48 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx';
 import LogIn from './pages/LogIn.jsx';
 import SignUp from './pages/SignUp.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+import Profile from './pages/Profile.jsx';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/log-in" element={<LogIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-        
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
